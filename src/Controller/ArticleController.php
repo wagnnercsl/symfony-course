57da5b940d252a138f5352b440cc4d22cf0df4b4
Wagnner <wagnner.leivas@gmail.com>
2019-08-26 10:59:30 -0300

    Profiler next

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 	public function homepage()
 	{
@@ -15,10 +16,16 @@ class ArticleController
 
 	public function show($slug)
 	{
-		return new Response(sprintf('Future page to show the article : %s',
-			$slug
+		$comments = [
+			'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+		];
 
-		));
+		return $this->render('article/show.html.twig',[
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'comments' => $comments
+		]);
 	}
 
 }
